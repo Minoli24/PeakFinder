44938b64fc3494db79dcdba9d0b68195d39c47e9
Gimhana Deshapriya <gimhandeshapriya567@gmail.com>
2024-12-07 21:06:03 +0530

    Add reanimated configuration, update Android NDK version, and enhance App component with user authentication state management

--- App.tsx
+++ App.tsx
@@ -1,14 +1,34 @@
-import React from 'react';
+import React, {useEffect, useRef} from 'react';
 import {StyleSheet} from 'react-native';
 import {GestureHandlerRootView} from 'react-native-gesture-handler';
 import RootNavigation from './src/navigation/RootNavigation';
 import {NavigationContainer} from '@react-navigation/native';
 import {SafeAreaProvider} from 'react-native-safe-area-context';
+
+import './reanimatedConfig';
+import {useMMKVString} from 'react-native-mmkv';
+
+import useNavigationStateStore from './src/store/navigationStore';
 function App(): React.JSX.Element {
+  const [userId, setUserid] = useMMKVString('userId');
+  const [userName, setName] = useMMKVString('userName');
+
+  const {setIsAuthenticated, setIsNavigationReady, setUsername} =
+    useNavigationStateStore();
+  useEffect(() => {
+    if (userId) {
+      setIsAuthenticated(true);
+      //@ts-ignore
+      setUsername(userName);
+    }
+    setIsNavigationReady(true);
+  }, [userId]);
+  const navRef = useRef();
+
   return (
     <GestureHandlerRootView style={styles.container}>
       <SafeAreaProvider>
-        <NavigationContainer>
+        <NavigationContainer ref={navRef}>
           <RootNavigation />
         </NavigationContainer>
       </SafeAreaProvider>
