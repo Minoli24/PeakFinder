8178c1358caedcb826e6d60ab7fd4bf10c88b450
Duhun De Silva <sandaruduhun@gmail.com>
2024-12-08 14:23:42 +0530

    build(deps): update App.tsx and package-lock.json

--- my-app/App.tsx
+++ my-app/App.tsx
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>welcome to my app duhun !..............</Text>
+      <Text>welcome to my app</Text>
       <StatusBar style="auto" />
     </View>
   );
