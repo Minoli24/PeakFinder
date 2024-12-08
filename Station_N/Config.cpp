bd90808c07c463fabced8105b9654061e03409d4
Minoli Rashmitha <99578555+Minoli24@users.noreply.github.com>
2024-12-08 16:11:51 +0530

    Update Config.cpp

--- Station_N/Config.cpp
+++ Station_N/Config.cpp
@@ -4,6 +4,7 @@ TinyGPSPlus gps; // Initialize the GPS module using the TinyGPSPlus library
 SoftwareSerial gpsSerial(GPS_RX_PIN, GPS_TX_PIN); // Define a SoftwareSerial object for GPS communication, using the specified RX and TX pins
 
 // Define other global variables here 
+// Initialize the OLED display with specified width, height, and I2C configuration
 Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);
 
 
