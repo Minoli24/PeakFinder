c001cd2cadd321fb993f408c0dc56fa40956340f
Minoli Rashmitha <99578555+Minoli24@users.noreply.github.com>
2024-12-08 16:07:32 +0530

    Update GPS.cpp

--- Station_N/GPS.cpp
+++ Station_N/GPS.cpp
@@ -3,5 +3,5 @@
 // Function to initialize the GPS module
 void setupGPS() {
     gpsSerial.begin(GPS_BAUD); // Start communication with the GPS module at the defined baud rate
-    Serial.println("GPS Module Initialized");
+    Serial.println("GPS Module Initialized"); // Output a message indicating successful initialization
 }
