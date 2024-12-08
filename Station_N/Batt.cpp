cafd397c78d9305b4351ab348d2a11e68bb7bb03
Minoli Rashmitha <99578555+Minoli24@users.noreply.github.com>
2024-12-08 16:02:46 +0530

    Update Batt.cpp

--- Station_N/Batt.cpp
+++ Station_N/Batt.cpp
@@ -9,6 +9,6 @@ const float VOLTAGE_DIVIDER_RATIO = MAX_BATTERY_VOLTAGE / PIN_VOLTAGE;
 // Parameters for stable reading 
 static float smoothedVoltage = 0;
 static const int REQUIRED_SAMPLES = 10;     // Number of consistent readings needed before changing 
-static const float VOLTAGE_THRESHOLD = 0.1; // Minimum voltage change to consider updating
+static const float VOLTAGE_THRESHOLD = 0.1; // Minimum voltage change to consider updating 
 static int consistentReadings = 0;
 static bool isInitialized = false;
