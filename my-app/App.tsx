4817323cf50298eec78887d0eab1ee723cb59a96
Duhun De Silva <sandaruduhun@gmail.com>
2024-12-09 10:28:02 +0530

    chore: delete CameraComponent.tsx

--- my-app/App.tsx
+++ my-app/App.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { StyleSheet, View } from 'react-native';
-import CameraComponent from './components/CameraComponent';
+import CameraComponent from './components/CameraScreen';
 
 export default function App() {
   return (
