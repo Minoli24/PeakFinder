97d897b199193158609e1c7e245837d722c122ad
Gimhana Deshapriya <gimhandeshapriya567@gmail.com>
2024-12-09 13:02:18 +0530

    Remove unused route names to simplify navigation configuration

--- src/navigation/config/routeNames.ts
+++ src/navigation/config/routeNames.ts
@@ -1,14 +1,4 @@
 export const routeNames = {
   Login: 'Login',
   Register: 'Register',
-  Home: 'Homeds',
-  MountainDetails: 'MountainDetails',
-  PathDetails: 'PathDetails',
-  PathDetailsAdd: 'PathDetailsAdd',
-
-  PathMapView: 'PathMapView',
-  PathCreateScreen: 'PathCreateScreen',
-  HistoryScreen: 'HistoryScreen',
-  ProfileScreen: 'ProfileScreen',
-  SettingScreen: 'SettingScreen',
 };
