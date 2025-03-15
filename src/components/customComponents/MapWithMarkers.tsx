9a74abb729b73d50ac7c02a9e249d23eef2de84d
Gimhana Deshapriya <gimhandeshapriya567@gmail.com>
2025-03-15 16:12:32 +0530

    Add rating display and additional info overlays in MapWithMarkers component

--- src/components/customComponents/MapWithMarkers.tsx
+++ src/components/customComponents/MapWithMarkers.tsx
@@ -4,6 +4,7 @@ import MapView, {Marker, Polyline, PROVIDER_GOOGLE} from 'react-native-maps';
 import {colors} from '../../theme/colors';
 import {Icon} from 'react-native-paper';
 import CustomMarker from './CustomMarker';
+import {calculateAverageRating} from '../../utils/calculateAverageRating';
 
 const MapWithMarkers = ({
   flex = 1, // Default flex value
@@ -13,10 +14,16 @@ const MapWithMarkers = ({
   markers: initialMarkers = [],
   overlayText = 'Top Right Text',
   onNavigate = () => {},
+  onRatingPress = () => {},
+  rating = 0,
+  showInfo = true,
+  showRating = true,
 }) => {
   const [dragStart, setDragStart] = useState(false);
   const [markers, setMarkers] = useState(initialMarkers);
-  const [pathCoordinates, setPathCoordinates] = useState(initialMarkers);
+  const [pathCoordinates, setPathCoordinates] = useState(
+    initialMarkers.filter(marker => !marker.isDevice), // Filter out markers with isDevice: true
+  );
   console.log('II', initialMarkers);
 
   const mm = [{latitude: 6.8154, longitude: 80.3815, isEndPoint: true}];
@@ -56,6 +63,8 @@ const MapWithMarkers = ({
             <CustomMarker
               markerIndex={item.markerIndex}
               isEndPoint={item.isEndPoint}
+              isSpecialPlace={item.isSpecialPlace}
+              isDevice={item?.isDevice}
             />
           </Marker>
         ))}
@@ -73,6 +82,58 @@ const MapWithMarkers = ({
       <TouchableOpacity style={styles.overlay} onPress={onNavigate}>
         <Icon source="crop-free" size={20} />
       </TouchableOpacity>
+      {showRating && (
+        <TouchableOpacity style={styles.overlay2} onPress={onRatingPress}>
+          <View
+            style={{
+              flexDirection: 'row',
+              gap: 6,
+              alignItems: 'center',
+            }}>
+            <Text
+              style={{
+                color: 'black',
+              }}>
+              {calculateAverageRating(rating)}
+            </Text>
+            <Icon source="star" size={20} color="#FFC403" />
+          </View>
+        </TouchableOpacity>
+      )}
+
+      {showInfo && (
+        <View style={styles.overlay3}>
+          <View
+            style={{
+              flexDirection: 'row',
+              alignItems: 'center',
+              marginBottom: 10,
+            }}>
+            <Icon source={'image-marker'} size={25} color={'red'} />
+            <Text
+              style={{
+                color: 'black',
+              }}>
+              {' '}
+              - Special Place
+            </Text>
+          </View>
+          <View
+            style={{
+              flexDirection: 'row',
+              alignItems: 'center',
+            }}>
+            <Icon source={'cellphone-wireless'} size={25} color={'red'} />
+            <Text
+              style={{
+                color: 'black',
+              }}>
+              {' '}
+              - Forest Reserve Device
+            </Text>
+          </View>
+        </View>
+      )}
     </View>
   );
 };
@@ -91,6 +152,28 @@ const styles = StyleSheet.create({
     paddingHorizontal: 10,
     paddingVertical: 5,
     borderRadius: 5,
+  },
+  overlay2: {
+    position: 'absolute',
+    top: 10,
+
+    zIndex: 10,
+    backgroundColor: 'rgba(255, 255, 255, 0.7)',
+    paddingHorizontal: 10,
+    paddingVertical: 5,
+    borderRadius: 5,
+    left: 10,
+  },
+  overlay3: {
+    position: 'absolute',
+    top: 70,
+
+    zIndex: 10,
+    backgroundColor: 'rgba(255, 255, 255, 0.7)',
+    paddingHorizontal: 10,
+    paddingVertical: 5,
+    borderRadius: 5,
+    left: 10,
   },
   overlayText: {
     fontSize: 14,
