35be38aecade0fc241d3bd29528dd6b0c25ea98b
Gimhana Deshapriya <gimhandeshapriya567@gmail.com>
2025-03-15 15:03:19 +0530

    Add boot splash screen initialization and update navigation structure

--- src/navigation/config/routeNames.ts
+++ src/navigation/config/routeNames.ts
@@ -1,4 +1,14 @@
 export const routeNames = {
   Login: 'Login',
   Register: 'Register',
+  Home: 'Homeds',
+  MountainDetails: 'MountainDetails',
+  PathDetails: 'PathDetails',
+  PathDetailsAdd: 'PathDetailsAdd',
+
+  PathMapView: 'PathMapView',
+  PathCreateScreen: 'PathCreateScreen',
+  HistoryScreen: 'HistoryScreen',
+  ProfileScreen: 'ProfileScreen',
+  SettingScreen: 'SettingScreen',
 };
