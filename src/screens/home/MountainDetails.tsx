970ce7d889010cb15228381a3300803cfc3fcb8a
Gimhana Deshapriya <gimhandeshapriya567@gmail.com>
2025-03-15 16:11:13 +0530

    Add distance filtering and bottom sheet modal to MountainDetails screen

--- src/screens/home/MountainDetails.tsx
+++ src/screens/home/MountainDetails.tsx
@@ -1,5 +1,11 @@
-import React from 'react';
-import {View, StyleSheet, Image, FlatList} from 'react-native';
+import React, {useCallback, useEffect, useMemo, useRef, useState} from 'react';
+import {
+  View,
+  StyleSheet,
+  Image,
+  FlatList,
+  TouchableOpacity,
+} from 'react-native';
 import ScrollViewWrapper from '../../components/wrappers/ScrollViewWrapper';
 import SafeAreaWrapper from '../../components/wrappers/SafeAreaWrapper';
 import ContentWrap from '../../components/wrappers/ContentWrap';
@@ -11,10 +17,74 @@ import {colors} from '../../theme/colors';
 import MountainCard from '../../components/customComponents/MountainCard';
 import {useCustomNavigation} from '../../hooks/useCustomNavigation';
 import {routeNames} from '../../navigation/config/routeNames';
+import {
+  BottomSheetModal,
+  BottomSheetModalProvider,
+  BottomSheetView,
+} from '@gorhom/bottom-sheet';
+import Slider from '@react-native-community/slider';
+import ContainedButton from '../../components/customComponents/ContainedButton';
 
 const MountainDetails = ({route}: any) => {
   const {mountainData} = route.params;
   const navigation = useCustomNavigation();
+  const snapPoints = useMemo(() => ['40%'], []);
+  const bottomSheetModalRef = useRef<BottomSheetModal>(null);
+  const handleSheetChanges = useCallback((index: number) => {
+    console.log('handleSheetChanges', index);
+    if (index == -1) {
+      //   setBannerVisible(false);
+    }
+  }, []);
+
+  const handlePresentModalPress = useCallback(() => {
+    console.log('Presss');
+    // setMarkerModalOpen(true);
+    bottomSheetModalRef.current?.present();
+  }, []);
+
+  const [distance, setDistance] = useState(0);
+  const [pathList, setPathList] = useState([]);
+  const [shortestPaths, setShortestPaths] = useState([]);
+
+  console.log('MMM', mountainData.paths);
+  const filterPathsByDistance = () => {
+    // Filter paths where distance is less than or equal to maxDistance
+    const filteredPaths = mountainData.paths.filter(path => {
+      const pathDistance = parseFloat(path.distance); // Convert distance to a number
+      return !isNaN(pathDistance) && pathDistance <= distance; // Check if distance is less than or equal to maxDistance
+    });
+    setPathList(() => filteredPaths);
+    bottomSheetModalRef.current?.close();
+
+    // You can return or use the filteredPaths variable as needed
+  };
+
+  const findShortestPath = paths => {
+    // Filter paths with numeric distances (ignoring "km (one way)" or similar text)
+    const validPaths = paths.filter(path => !isNaN(parseFloat(path.distance)));
+
+    // Find the shortest distance
+    const shortestDistance = Math.min(
+      ...validPaths.map(path => parseFloat(path.distance)),
+    );
+
+    // Filter all paths that have the shortest distance
+    const shortestPaths = validPaths.filter(
+      path => parseFloat(path.distance) === shortestDistance,
+    );
+
+    // Return an array of pathIds with the shortest distance
+    return shortestPaths.map(path => path.pathId);
+  };
+
+  useEffect(() => {
+    if (mountainData?.paths) {
+      console.log('Shortest', findShortestPath(mountainData.paths));
+      setShortestPaths(findShortestPath(mountainData.paths));
+      setPathList(mountainData?.paths);
+    }
+  }, [mountainData]);
 
   return (
     <ScrollViewWrapper isScrollable={false}>
@@ -63,12 +133,19 @@ const MountainDetails = ({route}: any) => {
             <Text fontSize={18} variant="semiBold">
               Discover Paths
             </Text>
-            <Icon source="tune" size={20} />
+            <TouchableOpacity onPress={handlePresentModalPress}>
+              <Icon source="tune" size={20} />
+            </TouchableOpacity>
           </View>
           <FlatList
-            data={mountainData?.paths}
+            data={pathList}
             renderItem={({item}) => (
               <MountainCard
+                pathId={item.pathId}
+                shortestPaths={shortestPaths}
+                isPath={true}
+                hasAnimals={item?.hasAnimals && item.hasAnimals == 'Yes'}
+                hasWater={item.hasWater && item.hasWater == 'Yes'}
                 onPress={() =>
                   navigation.navigate(routeNames.PathDetails, {
                     pathItem: item,
@@ -96,6 +173,72 @@ const MountainDetails = ({route}: any) => {
           console.log('FAB Pressed!');
         }}
       />
+      <BottomSheetModal
+        ref={bottomSheetModalRef}
+        snapPoints={snapPoints}
+        onChange={handleSheetChanges}>
+        <BottomSheetView style={styles.contentContainer}>
+          <View
+            style={{
+              flexDirection: 'row',
+              justifyContent: 'space-between',
+            }}>
+            <Text
+              style={{
+                // textAlign: 'center',
+                fontWeight: '800',
+              }}>
+              Trail Distance
+            </Text>
+            <Text
+              style={{
+                // textAlign: 'center',
+                fontWeight: '800',
+              }}>
+              {distance} KM
+            </Text>
+          </View>
+
+          <View>
+            <Slider
+              style={{width: '100%', height: 40}}
+              minimumValue={1}
+              maximumValue={10}
+              minimumTrackTintColor={colors.primaryGreen}
+              maximumTrackTintColor="#000000"
+              onValueChange={val => setDistance(Math.round(val))}
+              thumbTintColor={colors.primaryGreen}
+              value={distance}
+            />
+            <View
+              style={{
+                flexDirection: 'row',
+                justifyContent: 'space-between',
+              }}>
+              <Text
+                style={{
+                  // textAlign: 'center',
+                  fontWeight: '800',
+                }}>
+                0 KM
+              </Text>
+              <Text
+                style={{
+                  // textAlign: 'center',
+                  fontWeight: '800',
+                }}>
+                10 KM
+              </Text>
+            </View>
+          </View>
+          <View
+            style={{
+              marginTop: 20,
+            }}>
+            <ContainedButton label="Filter" onPress={filterPathsByDistance} />
+          </View>
+        </BottomSheetView>
+      </BottomSheetModal>
     </ScrollViewWrapper>
   );
 };
@@ -109,6 +252,10 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     borderRadius: 15, // Rounded corners for the carousel item
     overflow: 'hidden',
+  },
+  contentContainer: {
+    flex: 1,
+    padding: 10,
   },
   carouselImage: {
     width: 350,
