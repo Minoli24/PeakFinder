f2660080b722bbcfb892d24a0227595867edc82c
Minoli Rashmitha <99578555+Minoli24@users.noreply.github.com>
2025-03-17 16:07:20 +0530

    Create colors.ts

---
+++ PeakFinderAdmin/src/theme/colors.ts
@@ -0,0 +1,11 @@
+export const colors = {
+  primary: '#984F2F',
+  primaryGreen: '#00BF63',
+  white: '#FFFFFF',
+  black: '#272728',
+  primaryBlack: '#000000',
+  tabBarIconColor: '#484C52',
+  onboardingButtonText: '#1E4542',
+  grey: '#9F9F9F',
+  whiteOpacity85: '#F2FFFFFF',
+};
