319046ea99a9448a48fc94d68255619b4b3199f2
Minoli Rashmitha <99578555+Minoli24@users.noreply.github.com>
2025-03-17 09:25:17 +0530

    Create onBoardingStack.tsx

---
+++ PeakFinderAdmin/src/navigation/stacks/onboarding/OnBoardingStack.tsx
@@ -0,0 +1,25 @@
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
+import OnBoardingScreenOne from '../../../screens/onboarding/OnBoardingScreenOne';
+import {screenOptions} from '../../config/navigationConstants';
+import OnBoardingScreenTwo from '../../../screens/onboarding/OnBoardingScreenTwo';
+
+const OnBoardingStackNav = createNativeStackNavigator();
+
+export default function OnBoardingStack() {
+  return (
+    <OnBoardingStackNav.Navigator>
+      <OnBoardingStackNav.Screen
+        component={OnBoardingScreenOne}
+        name="OnboardingOne"
+        //@ts-ignore
+        options={{...screenOptions, animation: 'slide_from_right'}}
+      />
+      <OnBoardingStackNav.Screen
+        component={OnBoardingScreenTwo}
+        name="OnboardingTwo"
+        //@ts-ignore
+        options={{...screenOptions, animation: 'slide_from_right'}}
+      />
+    </OnBoardingStackNav.Navigator>
+  );
+}
